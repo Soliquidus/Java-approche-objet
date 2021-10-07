cc64af6e947ff159b06321df98c81113335d4605
Soliquidus <tibo.pfeifer@gmail.com>
2021-10-07 12:11:22 +0200

    TP 7 - Héritage et constructeurs

--- src/fr/diginamic/banque/TestBanque.java
+++ src/fr/diginamic/banque/TestBanque.java
@@ -1,10 +1,15 @@
 package fr.diginamic.banque;
 
 import fr.diginamic.banque.entites.Compte;
+import fr.diginamic.banque.entites.CompteTaux;
 
 public class TestBanque {
     public static void main(String[] args) {
-        Compte compte = new Compte(105_484_987_897L, 350_000);
-        System.out.println(compte);
+        Compte compteNormal = new Compte(105_484_987_897L, 350_000);
+        CompteTaux compteTaux = new CompteTaux(105_484_987_900L, 100_000_000, 10000);
+        Compte [] comptes = {compteNormal, compteTaux};
+        for (Compte compte : comptes) {
+            System.out.println(compte);
+        }
     }
 }
