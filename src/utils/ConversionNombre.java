f5b167dbcec70a7b9e35a0a58490454cc05d8b00
Soliquidus <tibo.pfeifer@gmail.com>
2021-10-07 11:29:20 +0200

    TP 4 - Utilisation de methodes static existantes

---
+++ src/utils/ConversionNombre.java
@@ -0,0 +1,14 @@
+package utils;
+
+public class ConversionNombre {
+    public static void main(String[] args) {
+        String chaine = "12";
+        int chaineConvertie = Integer.parseInt(chaine);
+        System.out.println(chaineConvertie);
+
+        int a = 1021657;
+        int b = 1021660;
+
+        System.out.println(Integer.max(a, b));
+    }
+}
